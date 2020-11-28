a5435cbba059f6d76959a3c276daf97f9c20193b
Dimas <123@mail.com>
2020-11-28 19:50:27 +0200

    Full user page

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,10 +3,36 @@
 @section('content')
   <div class="col">
 
+    <h1>Show user's detail</h1>
+
     <dl class="row">
       <dt class="col-sm-3">Name</dt>
       <dd class="col-sm-9">{{ $user->name }}</dd>
     </dl>
-
+    <dl class="row">
+      <dt class="col-sm-3">First name</dt>
+      <dd class="col-sm-9">{{ $user->first_name }}</dd>
+    </dl>
+    <dl class="row">
+      <dt class="col-sm-3">Last name</dt>
+      <dd class="col-sm-9">{{ $user->last_name }}</dd>
+    </dl>
+    <dl class="row">
+      <dt class="col-sm-3">Role</dt>
+      <dd class="col-sm-9">{{ $user->role }}</dd>
+    </dl>
+    <dl class="row">
+      <dt class="col-sm-3">Gender</dt>
+      <dd class="col-sm-9">{{ $user->gender }}</dd>
+    </dl>
+    <dl class="row">
+      <dt class="col-sm-3">Salary</dt>
+      <dd class="col-sm-9">{{ $user->salary }}</dd>
+    </dl>
+    <dl class="row">
+      <dt class="col-sm-3">Email</dt>
+      <dd class="col-sm-9">{{ $user->email }}</dd>
+    </dl>
+    <a href="{{ route('users.index') }}" class="btn btn-primary">Back to users</a>
   </div>
 @endsection
